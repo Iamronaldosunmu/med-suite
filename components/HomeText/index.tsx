a66b7561a5f646ac8e963dbfe6156ca78e2e9484
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-08 22:43:26 +0100

    Finished working on the home text section

---
+++ components/HomeText/index.tsx
@@ -0,0 +1,19 @@
+import ButtonGroup from "../ButtonGroup";
+import styles from "./HomeText.module.css";
+
+const HomeText = () => {
+  return (
+    <article className={styles.textContainer}>
+      <h1 className={styles.header}>Visa-sponsored jobs for Nigerian Nurses</h1>
+      <img className={styles.circleImage} src="/icons/circles.svg" />
+      <p className={styles.subtext}>
+        We connect nurses all around Nigeria with Hospitals all over the world
+        and take care of their relocation and visa-sponsorship. If you’re a
+        nurse with over two years of industry experience, you should definitely
+        apply.
+      </p>
+      <ButtonGroup />
+    </article>
+  );
+};
+export default HomeText;
