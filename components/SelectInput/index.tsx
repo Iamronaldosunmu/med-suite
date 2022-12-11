bb02833152c9986d6a492221e1577cab27af7851
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-11 17:47:19 +0100

    Finished working on the select input component

---
+++ components/SelectInput/index.tsx
@@ -0,0 +1,34 @@
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
+import styles from "./SelectInput.module.css";
+
+interface SelectInputProps {
+  label: string;
+  setValue: Dispatch<SetStateAction<string>>;
+  value: string;
+  options?: string[];
+}
+
+const SelectInput: React.FC<SelectInputProps> = ({
+  label,
+  value,
+  setValue,
+  options,
+}) => {
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setValue(e.target.value);
+  };
+  return (
+    <div className={styles.container}>
+      <label htmlFor={label} className={styles.label}>
+        {label}
+      </label>
+      <select onChange={onChange} id={label} value={value} className={styles.input}>
+        <option value=""></option>
+        {options?.map((option) => (
+          <option value={option}>{option}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+export default SelectInput;
