33c0954dc89113743b607c0c5c12e921302f5164
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-11 17:45:48 +0100

    Finished working on the button group comoonent

--- components/ButtonGroup/index.tsx
+++ components/ButtonGroup/index.tsx
@@ -1,10 +1,16 @@
+import { useRouter } from "next/router";
 import styles from "./ButtonGroup.module.css";
 
 const ButtonGroup = () => {
+  const router = useRouter();
   return (
     <div className={styles.buttonGroupContainer}>
-      <button className={`${styles.button} ${styles.purple}`}>Start Application</button>
-      <button className={`${styles.button} ${styles.orange}`}>Continue Application</button>
+      <button onClick={() => router.push("/signup")} className={`${styles.button} ${styles.purple}`}>
+        Start Application
+      </button>
+      <button className={`${styles.button} ${styles.orange}`}>
+        Continue Application
+      </button>
     </div>
   );
 };
