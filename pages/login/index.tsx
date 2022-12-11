9827c86330455139036f1338aaf81bd70e826265
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-11 17:49:20 +0100

    Finished working on the login page

---
+++ pages/login/index.tsx
@@ -0,0 +1,55 @@
+import { NextPage } from "next";
+import Head from "next/head";
+import { useState } from "react";
+import InputGroup from "../../components/InputGroup";
+import Logo from "../../components/Logo";
+import Navbar from "../../components/Navbar";
+import styles from "../../styles/Signup.module.css";
+
+const Signup: NextPage = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  return (
+    <main className={styles.main}>
+      <Head>
+        <title>Med Suite - Create an Account</title>
+      </Head>
+      <div className={styles.navcontainer}>
+        <Navbar />
+      </div>
+      <div className={styles.pageContainer}>
+        <section className={styles.imageContainer}>
+          <img className={styles.bgImage} src="/images/nursebg.png" />
+          <div className={styles.logo}>
+            <Logo />
+          </div>
+        </section>
+        <section className={styles.navTextContainer}>
+          <div>
+            <h1 className={styles.navTitle}>Login to Your Account</h1>
+            <form className={styles.signupForm}>
+              <section className={styles.inputsContainer}>
+                <InputGroup
+                  label="Email address"
+                  setValue={setEmail}
+                  value={email}
+                />
+                <InputGroup
+                  password
+                  label="Password"
+                  setValue={setPassword}
+                  value={password}
+                />
+              </section>
+              <button className={styles.signupButton}>Login</button>
+            </form>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Signup;
