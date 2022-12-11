d7e0982aa4c339d77ef04a6ac9348fc9b62def09
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-11 17:49:35 +0100

    Finished working on the signup page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,16 +1,65 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import InputGroup from "../../components/InputGroup";
+import Logo from "../../components/Logo";
+import Navbar from "../../components/Navbar";
+import styles from "../../styles/Signup.module.css";
 
+const Signup: NextPage = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const router = useRouter();
 
-const Signup : NextPage = () => {
-    return (
-        <main>
-            <Head>
-                <title>Med Suite - Create an Account</title>
-            </Head>
-            
-        </main>
-    )
-}
+  return (
+    <main className={styles.main}>
+      <Head>
+        <title>Med Suite - Create an Account</title>
+      </Head>
+      <div className={styles.navcontainer}>
+        <Navbar />
+      </div>
+      <div className={styles.pageContainer}>
+        <section className={styles.imageContainer}>
+          <img className={styles.bgImage} src="/images/nursebg.png" />
+          <div className={styles.logo}>
+            <Logo />
+          </div>
+        </section>
+        <section className={styles.navTextContainer}>
+          <div>
+            <h1 className={styles.navTitle}>Create an Account</h1>
+            <form className={styles.signupForm}>
+              <section className={styles.inputsContainer}>
+                <InputGroup
+                  label="Email address"
+                  setValue={setEmail}
+                  value={email}
+                />
+                <InputGroup
+                  password
+                  label="Password"
+                  setValue={setPassword}
+                  value={password}
+                />
+                <InputGroup
+                  password
+                  label="Confirm Password"
+                  setValue={setConfirmPassword}
+                  value={confirmPassword}
+                />
+              </section>
+              <button onClick={() => router.push("/application_form/contact_details")} className={styles.signupButton}>
+                Create account
+              </button>
+            </form>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+};
 
 export default Signup;
