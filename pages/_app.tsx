a14abf6a67a0ffb7c066f8d79f4e71dd85e9089a
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-12 19:23:50 +0100

    Added fix for unstyled text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,26 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
 
+import Router from "next/router";
+
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]');
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute("media");
+    });
+  };
+  tempFix();
+};
+
+Router.events.on("routeChangeComplete", routeChange );
+Router.events.on("routeChangeStart", routeChange);
+
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <AnimatePresence>
