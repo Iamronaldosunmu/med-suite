7d863934e0f81a0e33112b7f742a7293effbc8bc
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-13 14:01:04 +0100

    added the onBlur and type props

--- components/InputGroup/index.tsx
+++ components/InputGroup/index.tsx
@@ -1,22 +1,53 @@
 import { ChangeEvent, Dispatch, SetStateAction } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import styles from "./InputGroup.module.css";
 
 interface InputGroupProps {
-    label: string; 
-    setValue: Dispatch<SetStateAction<string>>;
-    value: string;
-    password?: boolean;
-    }
-const InputGroup: React.FC<InputGroupProps> = ({ label, value, setValue, password }) => {
-    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setValue(e.target.value);
-    }
-    return (
-        <div className={styles.container}>
-            <label htmlFor={label} className={styles.label}>{label}</label>
-            <input id={label} onChange={onChange} value={value} className={styles.input} type={password ? "password": "text"} />
-        </div>
-    )
+  label: string;
+  setValue: Dispatch<SetStateAction<string>>;
+  value: string;
+  error?: string;
+  onBlur?: (e: ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
 }
+const InputGroup: React.FC<InputGroupProps> = ({
+  label,
+  value,
+  setValue,
+  error,
+  onBlur,
+  type,
+}) => {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  return (
+    <div className={styles.container}>
+      <label htmlFor={label} className={styles.label}>
+        {label}
+      </label>
+      <input
+        id={label}
+        onChange={onChange}
+        type={type ? type : "text"}
+        value={value}
+        className={styles.input}
+        onBlur={onBlur}
+      />
+      <AnimatePresence>
+        {error && (
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={styles.error}
+          >
+            {error}
+          </motion.p>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
 
 export default InputGroup;
