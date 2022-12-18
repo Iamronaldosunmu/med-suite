93764d42077ccc4c6c4d9de39b24ca22da726830
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-18 20:57:36 +0100

    Finished customizing the button component and added the onClick and loading props

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,23 +1,51 @@
 import Link from "next/link";
+import { MouseEventHandler } from "react";
+import LottieAnimation from "../Lottie";
 import styles from "./Button.module.css";
+import loader from "../../components/Lottie/loader.json";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface ButtonProps {
   text: string;
-  path: string;
-    orange?: boolean;
-    color?: string;
+  orange?: boolean;
+  color?: string;
+  onClick?: any;
+  loading?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, path, orange, color }) => {
+const Button: React.FC<ButtonProps> = ({
+  text,
+  orange,
+  color,
+  onClick,
+  loading,
+}) => {
   return (
-    <Link href={path}>
-      <button
-        style={{ backgroundColor: color?  color : (orange ? "#FF6B00" : "#7777D6") }}
-        className={styles.button}
-      >
-        {text}
-      </button>
-    </Link>
+    // <Link href={path}>
+    <button
+      onClick={onClick ? onClick : () => {}}
+      style={{
+        backgroundColor: color ? color : orange ? "#FF6B00" : "#7777D6",
+      }}
+      className={styles.button}
+    >
+      <AnimatePresence>
+        {!loading && (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            // layout
+          >
+            {text}
+          </motion.span>
+        )}
+        {loading && (
+          <LottieAnimation width={56} height={56} animationData={loader} />
+        )}
+      </AnimatePresence>
+    </button>
+    // </Link>
   );
 };
 
