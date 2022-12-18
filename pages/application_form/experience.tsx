ea01292db7be02390e028aac7931003ff47d9302
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-18 21:29:40 +0100

    Finished working on the experience section

--- pages/application_form/experience.tsx
+++ pages/application_form/experience.tsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
+import { string } from "joi";
+import Router, { useRouter } from "next/router";
+import { FormEvent, useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import Button from "../../components/Button";
 import FadeIn from "../../components/FadeIn";
 import FileUpload from "../../components/FileUpload";
+import InfoMessage from "../../components/InfoMessage";
 import Logo from "../../components/Logo";
 import ProgressBar from "../../components/ProgressBar";
 import SelectInput from "../../components/SelectInput";
 import styles from "../../styles/Form.module.css";
+import DocumentProps from "../../Types/Document";
+import {
+  validateExperienceInputs,
+  validateTextField,
+} from "../../utilities/TextFieldValidation";
+import client from "../api/Services/AxiosClient";
+
+interface ExperienceFormErrors {
+  nursingExperience?: string;
+  postGraduateExperience?: string;
+  proofOfWork?: string;
+}
+
+const defaultProofOfWork = {
+  secure_url: "",
+  public_id: "",
+  fileName: "",
+};
 
 const Experience = () => {
   const experienceOptions = [
@@ -15,9 +37,84 @@ const Experience = () => {
   ];
   const [nursingExperience, setNursingExperience] = useState("");
   const [postGraduateExperience, setPostGraduateExperience] = useState("");
-  const [proofOfWork, setProofOfWork] = useState<File>();
+  const [proofOfWork, setProofOfWork] =
+    useState<DocumentProps>(defaultProofOfWork);
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState<ExperienceFormErrors>({
+    nursingExperience: "",
+    postGraduateExperience: "",
+    proofOfWork: "",
+  });
+  const [cookies, setCookie, removeCookie] = useCookies(["user", "applicant"]);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!cookies.user || !cookies.applicant) router.push("/login");
+    else {
+      setNursingExperience(cookies.applicant.experience?.nursingExperience);
+      setPostGraduateExperience(
+        cookies.applicant.experience?.postGraduateExperience
+      );
+      setProofOfWork(cookies.applicant.experience?.proofOfWork);
+    }
+  }, []);
+
+  const saveExperience = async (redirectPath: string) => {
+    setErrors(
+      validateExperienceInputs({
+        nursingExperience,
+        postGraduateExperience,
+        proofOfWork,
+      })
+    );
+    if (
+      nursingExperience &&
+      postGraduateExperience &&
+      proofOfWork?.secure_url &&
+      Object.values(errors).every((value) => !Boolean(value))
+    ) {
+      setLoading(true);
+      try {
+        const payload = {
+          nursingExperience,
+          postGraduateExperience,
+          proofOfWork: {
+            secure_url: proofOfWork.secure_url,
+            public_id: proofOfWork.public_id,
+            fileName: proofOfWork.fileName,
+          },
+        };
+        const response = await client.post(
+          `/applicant/experience/${cookies.user.applicantId}`,
+          payload
+        );
+        router.push(redirectPath);
+      } catch (error: any) {
+        if (!error.response) alert("Please Check your internet Connection.");
+        else {
+          console.log(error);
+          alert(error.response?.data?.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+    console.log(errors);
+  };
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    await saveExperience("/application_form/application_fee");
+  };
+  const onGoBack = async (e: FormEvent) => {
+    e.preventDefault();
+    setCookie("applicant", {
+      ...cookies.applicant,
+      experience: { nursingExperience, postGraduateExperience, proofOfWork },
+    });
+    router.push("/application_form/documents");
+  };
   return (
-    <FadeIn noExit>
+    <FadeIn>
       <main>
         <div className={styles.logoContainer}>
           <Logo />
@@ -32,26 +129,62 @@ const Experience = () => {
               value={nursingExperience}
               setValue={setNursingExperience}
               options={experienceOptions}
+              error={errors.nursingExperience}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  nursingExperience: validateTextField(
+                    nursingExperience,
+                    "Nursing Experience"
+                  )
+                    ? validateTextField(nursingExperience, "Nursing Experience")
+                    : "",
+                })
+              }
             />
             <SelectInput
               label="Post-Graduate Working Experience"
               value={postGraduateExperience}
               setValue={setPostGraduateExperience}
               options={experienceOptions}
+              error={errors.postGraduateExperience}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  postGraduateExperience: validateTextField(
+                    postGraduateExperience,
+                    "Post-Graduate Experience"
+                  )
+                    ? validateTextField(
+                        postGraduateExperience,
+                        "Post-Graduate Experience"
+                      )
+                    : "",
+                })
+              }
             />
             <FileUpload
+              fieldName={"proofOfWork"}
+              error={errors.proofOfWork}
               file={proofOfWork}
               setFile={setProofOfWork}
+              onFileUpload={() => setErrors({ ...errors, proofOfWork: "" })}
               label="Proof of working experience in a hospital setting in the last 12 months"
             />
+          <InfoMessage text={"Kindly request for a letter of Good Standing from the Nursing and Midwifery Council in your country which will verify among other things, your nursing education and license. The letter should be addressed medsuiteofficial@gmail.com"} />
+          <InfoMessage text={"You may have to make a payment to the Nursing and Midwifery Council in your country to enable Dataflow Group initiate the verification request for your credential verification"} />
+
             <div>
+              <div>
+                <Button
+                  loading={loading}
+                  onClick={onSubmit}
+                  text="Next > Application Fee"
+                  orange
+                />
+              </div>
               <Button
-                path="/application_form/application_fee"
-                text="Next > Application Fee"
-                orange
-              />
-              <Button
-                path="/application_form/experience"
+                onClick={onGoBack}
                 text="Prev < Experience"
               />
             </div>
