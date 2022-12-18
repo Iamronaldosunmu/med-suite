f4e315a4194af6c35f34022924c2d5b3211cb481
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-18 20:59:18 +0100

    Finished working on the fade in component

--- components/FadeIn/index.tsx
+++ components/FadeIn/index.tsx
@@ -1,17 +1,21 @@
 import { motion } from "framer-motion";
 
 interface FadeInProps {
-    children: JSX.Element;
-    noExit?: boolean;
-    scaleIn?: boolean;
+  children: JSX.Element;
+  noExit?: boolean;
+  scaleIn?: boolean;
 }
 
 const FadeIn: React.FC<FadeInProps> = ({ children, noExit, scaleIn }) => {
   return (
     <motion.div
       initial={{ opacity: 0, scale: scaleIn ? 0.8 : 1 }}
-      animate={{ opacity: 1, scale: 1, transition: { duration: 0.3 } }}
-          exit={noExit ? {} : { opacity: 0,  transition: { duration: 0.3 } }}
+      animate={{
+        opacity: 1,
+        scale: 1,
+        transition: { duration: 0.4, delay: 0.3 },
+      }}
+      exit={noExit ? {} : { opacity: 0, transition: { duration: 0.4 } }}
     >
       {children}
     </motion.div>
