1adcf2d127e32408aabb801d50e666457e0158f3
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-18 21:40:36 +0100

    Created a lot of functions for validating inputs

--- utilities/TextFieldValidation.tsx
+++ utilities/TextFieldValidation.tsx
@@ -1,4 +1,5 @@
 import Joi from "joi";
+import DocumentProps from "../Types/Document";
 
 // Function to validate the text inputs
 export const validateTextField: any = (
@@ -109,6 +110,175 @@ export const validateLoginInputs = (payload: LoginPayloadInput) => {
           (item) => item.path[0] == "password"
         )?.message
       : "",
+  };
+
+  return result;
+};
+
+interface ContactDetailsPayloadInput {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  phoneNumber: string;
+  stateOfOrigin: string;
+  countryOfOrigin: string;
+  street: string;
+  city: string;
+  state: string;
+}
+
+export const validateContactDetailsInputs = (
+  payload: ContactDetailsPayloadInput
+) => {
+  const schema = Joi.object({
+    firstName: Joi.string().min(3).max(50).required().label("First Name"),
+    middleName: Joi.string().min(3).max(50).required().label("Middle Name"),
+    lastName: Joi.string().min(3).max(50).required().label("Last Name"),
+    phoneNumber: Joi.string().min(3).max(15).required().label("Phone Number"),
+    stateOfOrigin: Joi.string()
+      .min(3)
+      .max(50)
+      .required()
+      .label("State Of Orgin"),
+    countryOfOrigin: Joi.string()
+      .min(3)
+      .max(50)
+      .required()
+      .label("Country Of Origin"),
+    street: Joi.string().min(3).max(50).required().label("Street"),
+    city: Joi.string().min(3).max(50).required().label("City"),
+    state: Joi.string().min(3).max(50).required().label("State"),
+  });
+
+  const validationResult = schema.validate(payload, { abortEarly: false });
+
+  const result = {
+    firstName: validationResult.error
+      ? validationResult.error.details.find(
+          (item) => item.path[0] == "firstName"
+        )?.message
+      : "",
+    middleName: validationResult.error
+      ? validationResult.error.details.find(
+          (item) => item.path[0] == "middleName"
+        )?.message
+      : "",
+    lastName: validationResult.error
+      ? validationResult.error.details.find(
+          (item) => item.path[0] == "lastName"
+        )?.message
+      : "",
+    phoneNumber: validationResult.error
+      ? validationResult.error.details.find(
+          (item) => item.path[0] == "phoneNumber"
+        )?.message
+      : "",
+    stateOfOrigin: validationResult.error
+      ? validationResult.error.details.find(
+          (item) => item.path[0] == "stateOfOrigin"
+        )?.message
+      : "",
+    countryOfOrigin: validationResult.error
+      ? validationResult.error.details.find(
+          (item) => item.path[0] == "countryOfOrigin"
+        )?.message
+      : "",
+    street: validationResult.error
+      ? validationResult.error.details.find((item) => item.path[0] == "street")
+          ?.message
+      : "",
+    city: validationResult.error
+      ? validationResult.error.details.find((item) => item.path[0] == "city")
+          ?.message
+      : "",
+    state: validationResult.error
+      ? validationResult.error.details.find((item) => item.path[0] == "state")
+          ?.message
+      : "",
+  };
+
+  return result;
+};
+
+interface ExperienceInput {
+  nursingExperience: string;
+  postGraduateExperience: string;
+  proofOfWork?: DocumentProps;
+}
+
+export const validateExperienceInputs = (payload: ExperienceInput) => {
+  const schema = Joi.object({
+    nursingExperience: Joi.string()
+      .min(3)
+      .max(50)
+      .required()
+      .label("Nursing Experience"),
+    postGraduateExperience: Joi.string()
+      .min(3)
+      .max(50)
+      .required()
+      .label("Post Graduate Experience"),
+  });
+
+  const validationResult = schema.validate(
+    {
+      nursingExperience: payload.nursingExperience,
+      postGraduateExperience: payload.postGraduateExperience,
+    },
+    { abortEarly: false }
+  );
+  const result = {
+    nursingExperience: validationResult.error
+      ? validationResult.error.details.find(
+          (item) => item.path[0] == "nursingExperience"
+        )?.message
+      : "",
+    postGraduateExperience: validationResult.error
+      ? validationResult.error.details.find(
+          (item) => item.path[0] == "postGraduateExperience"
+        )?.message
+      : "",
+    proofOfWork: payload.proofOfWork!.secure_url
+      ? ""
+      : '"Proof Of Work" is not allowed to be empty',
+  };
+
+  return result;
+};
+
+interface DocumentInput {
+  nursingDegree: DocumentProps;
+  practicingLicense: DocumentProps;
+  photoIdentification: DocumentProps;
+  evidenceOfRegistration: DocumentProps;
+  resume: DocumentProps;
+  referenceLetter: DocumentProps;
+  birthCertificate: DocumentProps;
+}
+
+export const validateDocumentInputs = (payload: DocumentInput) => {
+  const result = {
+    nursingDegree: payload.nursingDegree!.secure_url
+      ? ""
+      : '"Nursing Degree" is not allowed to be empty',
+    practicingLicense: payload.practicingLicense!.secure_url
+      ? ""
+      : '"Practicing License" is not allowed to be empty',
+    photoIdentification: payload.photoIdentification!.secure_url
+      ? ""
+      : '"Photo Identification" is not allowed to be empty',
+    evidenceOfRegistration: payload.evidenceOfRegistration!.secure_url
+      ? ""
+      : '"Evidence Of Registration" is not allowed to be empty',
+    resume: payload.resume!.secure_url
+      ? ""
+      : '"Resume" is not allowed to be empty',
+    referenceLetter: payload.referenceLetter!.secure_url
+      ? ""
+      : '"Reference Letters" is not allowed to be empty',
+    birthCertificate: payload.birthCertificate!.secure_url
+      ? ""
+      : '"Birth Certificate" is not allowed to be empty',
   };
 
   return result;
