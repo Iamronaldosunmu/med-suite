8ead0d8306f1f62c2f85772417d5f9e6e804de74
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-18 21:28:06 +0100

    Finished working on the documents page

--- pages/application_form/contact_details.tsx
+++ pages/application_form/contact_details.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useContext, useRef, useState } from "react";
 import Button from "../../components/Button";
 import InputGroup from "../../components/InputGroup";
 import Logo from "../../components/Logo";
@@ -7,20 +7,140 @@ import SelectInput from "../../components/SelectInput";
 import styles from "../../styles/Form.module.css";
 import { states } from "../../components/SelectInput/StatesAndLga";
 import FadeIn from "../../components/FadeIn";
+import {
+  validateContactDetailsInputs,
+  validateTextField,
+} from "../../utilities/TextFieldValidation";
+import { ContactDetailsContext } from "../../components/Context/ContactDetailsContext";
+import client from "../api/Services/AxiosClient";
+import { UserContext } from "../../components/Context/UserContext";
+import Router, { useRouter } from "next/router";
+import { useCookies } from "react-cookie";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
+import inputStyles from "../../components/InputGroup/InputGroup.module.css";
+import { AnimatePresence, motion } from "framer-motion";
+import countriesData from "../../Data/countries.json";
+
+interface ContactDetailsFormErrors {
+  firstName?: string;
+  middleName?: string;
+  lastName?: string;
+  phoneNumber?: string;
+  stateOfOrigin?: string;
+  countryOfOrigin?: string;
+  street?: string;
+  city?: string;
+  state?: string;
+}
 
 const ContactDetails = () => {
-  const [firstName, setFirstName] = useState("");
-  const [middleName, setMiddleName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [stateOfOrigin, setStateOfOrigin] = useState("");
-  const [localGovernmentArea, setLocalGovernmentArea] = useState("");
+  const [cookies, setCookie, removeCookie] = useCookies(["user", "applicant"]);
+
+  const router = useRouter();
+  const [firstName, setFirstName] = useState(
+    cookies.applicant?.contactDetails?.firstName
+  );
+  const [middleName, setMiddleName] = useState(
+    cookies.applicant?.contactDetails?.middleName
+  );
+  const [lastName, setLastName] = useState(
+    cookies.applicant?.contactDetails?.lastName
+  );
+  const [phoneNumber, setPhoneNumber] = useState(
+    cookies.applicant?.contactDetails?.phoneNumber
+  );
+  const [stateOfOrigin, setStateOfOrigin] = useState(
+    cookies.applicant?.contactDetails?.stateOfOrigin
+  );
+  const [countryOfOrigin, setCountryOfOrigin] = useState(
+    cookies.applicant?.contactDetails?.countryOfOrigin
+  );
   // Address
-  const [street, setStreet] = useState("");
-  const [city, setCity] = useState("");
-  const [state, setState] = useState("");
+  const [street, setStreet] = useState(
+    cookies.applicant?.contactDetails?.street
+  );
+  const [city, setCity] = useState(cookies.applicant?.contactDetails?.city);
+  const [state, setState] = useState(cookies.applicant?.contactDetails?.state);
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState<ContactDetailsFormErrors>({
+    firstName: "",
+    middleName: "",
+    lastName: "",
+    phoneNumber: "",
+    stateOfOrigin: "",
+    countryOfOrigin: "",
+    city: "",
+    street: "",
+    state: "",
+  });
+
+  const updateCookies = async () => {
+    const { data: applicantData } = await client.get(
+      `/applicant/${cookies.user.applicantId}`
+    );
+    setCookie("applicant", applicantData.applicant);
+  };
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setErrors(
+      validateContactDetailsInputs({
+        firstName,
+        middleName,
+        lastName,
+        phoneNumber,
+        stateOfOrigin,
+        countryOfOrigin,
+        street,
+        city,
+        state,
+      })
+    );
+    if (
+      firstName &&
+      middleName &&
+      lastName &&
+      stateOfOrigin &&
+      countryOfOrigin &&
+      phoneNumber &&
+      street &&
+      city &&
+      state &&
+      Object.values(errors).every((value) => !Boolean(value))
+    ) {
+      try {
+        setLoading(true);
+        const payload = {
+          firstName,
+          middleName,
+          lastName,
+          phoneNumber,
+          stateOfOrigin,
+          countryOfOrigin,
+          street,
+          city,
+          state,
+        };
+        const { data } = await client.post(
+          `/applicant/contact_details/${cookies.user.applicantId}`,
+          payload
+        );
+        await updateCookies();
+        router.push("/application_form/documents");
+      } catch (error: any) {
+        console.log(error);
+        if (!error.response) alert("Please Check your internet Connection.");
+        else {
+          alert(error.response?.data?.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+
   return (
-    <FadeIn noExit>
+    <FadeIn>
       <main>
         <div className={styles.logoContainer}>
           <Logo />
@@ -34,50 +154,241 @@ const ContactDetails = () => {
               label="First Name"
               setValue={setFirstName}
               value={firstName}
+              error={errors.firstName}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  firstName: validateTextField(firstName, "First Name")
+                    ? validateTextField(firstName, "First Name")
+                    : "",
+                })
+              }
             />
             <InputGroup
               label="Middle Name"
               setValue={setMiddleName}
               value={middleName}
+              error={errors.middleName}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  middleName: validateTextField(middleName, "Middle Name")
+                    ? validateTextField(middleName, "Middle Name")
+                    : "",
+                })
+              }
             />
             <InputGroup
               label="Last Name"
               setValue={setLastName}
               value={lastName}
+              error={errors.lastName}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  lastName: validateTextField(lastName, "Last Name")
+                    ? validateTextField(lastName, "Last Name")
+                    : "",
+                })
+              }
             />
-            <InputGroup
+
+            {/* <InputGroup
               label="Phone Number"
               setValue={setPhoneNumber}
               value={phoneNumber}
-            />
-            <SelectInput
+              error={errors.phoneNumber}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  phoneNumber: validateTextField(phoneNumber, "Phone Number")
+                    ? validateTextField(phoneNumber, "Phone Number")
+                    : "",
+                })
+              }
+            /> */}
+            <style jsx global>
+              {`
+                .selected-flag.open {
+                  background: #ebebeb !important;
+                }
+                .flag-dropdown {
+                  background-color: #ebebeb !important;
+                }
+                .selected-flag:hover {
+                  background-color: #ebebeb !important;
+                }
+                .form-control {
+                  height: 51.4px;
+                }
+                @media screen and (max-width: 1024px) {
+                  .form-control {
+                    height: 42.4px !important;
+                    font-size: 14px !important;
+                  }
+                }
+              `}
+            </style>
+            <div className={inputStyles.container}>
+              <label className={inputStyles.label}>Phone Number</label>
+              <PhoneInput
+                country={"ng"}
+                value={phoneNumber}
+                onChange={(phone) => {
+                  setPhoneNumber(phone);
+                  console.log(phone);
+                }}
+                onBlur={() =>
+                  setErrors({
+                    ...errors,
+                    phoneNumber: validateTextField(phoneNumber, "Phone Number")
+                      ? validateTextField(phoneNumber, "Phone Number")
+                      : "",
+                  })
+                }
+                containerStyle={{ width: "100%" }}
+                inputStyle={{
+                  width: "100%",
+                  background: "#ebebeb",
+                  paddingLeft: 60,
+                  border: 0,
+                  borderRadius: 8,
+                }}
+                buttonStyle={{ border: 0, width: 50, left: 0, paddingLeft: 10 }}
+              />
+              <AnimatePresence>
+                {errors.phoneNumber && (
+                  <motion.p
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    className={inputStyles.error}
+                  >
+                    {errors.phoneNumber}
+                  </motion.p>
+                )}
+              </AnimatePresence>
+            </div>
+            <InputGroup
               label="State Of Origin"
               value={stateOfOrigin}
               setValue={setStateOfOrigin}
-              options={states.map((state) => state.name)}
+              error={errors.stateOfOrigin}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  stateOfOrigin: validateTextField(
+                    stateOfOrigin,
+                    "State Of Origin"
+                  )
+                    ? validateTextField(stateOfOrigin, "State Of Origin")
+                    : "",
+                })
+              }
             />
             <SelectInput
+              label="Country Of Origin"
+              value={countryOfOrigin}
+              setValue={setCountryOfOrigin}
+              options={Object.values(countriesData).map(
+                (country) => country["Country Name"]
+              )}
+              error={errors.countryOfOrigin}
+              onBlur={() => {
+                console.log(countryOfOrigin);
+                setErrors({
+                  ...errors,
+                  countryOfOrigin: validateTextField(
+                    countryOfOrigin,
+                    "Country Of Origin"
+                  )
+                    ? validateTextField(countryOfOrigin, "Country Of Origin")
+                    : "",
+                });
+              }}
+            />
+            {/* <SelectInput
               label="Local Government Area"
               value={localGovernmentArea}
               setValue={setLocalGovernmentArea}
               options={
                 states.find((state) => state.name == stateOfOrigin)?.lgas
               }
-            />
+              error={errors.localGovernmentArea}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  localGovernmentArea: validateTextField(
+                    localGovernmentArea,
+                    "Local Government Area"
+                  )
+                    ? validateTextField(
+                        localGovernmentArea,
+                        "Local Government Area"
+                      )
+                    : "",
+                })
+              }
+            /> */}
             <p className={styles.contactSection}>Address</p>
-            <InputGroup label="Street" setValue={setStreet} value={street} />
-            <InputGroup label="City" setValue={setCity} value={city} />
-            <SelectInput
-              label="State"
+            <InputGroup
+              label="Street"
+              setValue={setStreet}
+              value={street}
+              error={errors.street}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  street: validateTextField(street, "Street")
+                    ? validateTextField(street, "Street")
+                    : "",
+                })
+              }
+            />
+            <InputGroup
+              label="City"
+              setValue={setCity}
+              value={city}
+              error={errors.city}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  city: validateTextField(city, "City")
+                    ? validateTextField(city, "City")
+                    : "",
+                })
+              }
+            />
+            <InputGroup
+              label="State / Province of Residence"
+              value={state}
+              setValue={setState}
+              error={errors.state}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  state: validateTextField(state, "State")
+                    ? validateTextField(state, "State")
+                    : "",
+                })
+              }
+            />
+            {/* <SelectInput
+              label="State / Province of Residence"
               value={state}
               setValue={setState}
               options={states.map((state) => state.name)}
-            />
-            <Button
-              path="/application_form/documents"
-              text="Next > Documents"
-              orange
-            />
+              error={errors.state}
+              onBlur={() =>
+                setErrors({
+                  ...errors,
+                  state: validateTextField(state, "State")
+                    ? validateTextField(state, "State")
+                    : "",
+                })
+              }
+            /> */}
+            <Button onClick={onSubmit} text="Next > Documents" orange />
           </form>
         </section>
       </main>
