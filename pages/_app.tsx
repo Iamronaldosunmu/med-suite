0625bab3f9be468fb15cbb9b7fd10330bd6dc29f
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-18 21:04:46 +0100

    Added the Context and Cookies providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
 
 import Router from "next/router";
+import { CookiesProvider } from "react-cookie";
+import UserContextProvider from "../components/Context/UserContext";
+import ContactDetailsContextProvider from "../components/Context/ContactDetailsContext";
+import ContactDetails from "./application_form/contact_details";
 
 const routeChange = () => {
   // Temporary fix to avoid flash of unstyled content
@@ -19,13 +23,19 @@ const routeChange = () => {
   tempFix();
 };
 
-Router.events.on("routeChangeComplete", routeChange );
+Router.events.on("routeChangeComplete", routeChange);
 Router.events.on("routeChangeStart", routeChange);
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-    <AnimatePresence>
-      <Component {...pageProps} key={router.route} />
-    </AnimatePresence>
+    <CookiesProvider>
+      <UserContextProvider>
+        <ContactDetailsContextProvider>
+          <AnimatePresence>
+            <Component {...pageProps} key={router.route} />
+          </AnimatePresence>
+        </ContactDetailsContextProvider>
+      </UserContextProvider>
+    </CookiesProvider>
   );
 }
