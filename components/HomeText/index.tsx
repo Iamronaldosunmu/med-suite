16166f09a0f2753dad1c27816324c67d493d2f0c
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-18 21:00:27 +0100

    Edited the text on the landing page

--- components/HomeText/index.tsx
+++ components/HomeText/index.tsx
@@ -4,10 +4,10 @@ import styles from "./HomeText.module.css";
 const HomeText = () => {
   return (
     <article className={styles.textContainer}>
-      <h1 className={styles.header}>Visa-sponsored jobs for Nigerian Nurses</h1>
+      <h1 className={styles.header}>Visa-sponsored jobs for African Nurses</h1>
       <img className={styles.circleImage} src="/icons/circles.svg" />
       <p className={styles.subtext}>
-        We connect nurses all around Nigeria with Hospitals all over the world
+        We connect nurses all around Africa with Hospitals all over the world
         and take care of their relocation and visa-sponsorship. If you’re a
         nurse with over two years of industry experience, you should definitely
         apply.
