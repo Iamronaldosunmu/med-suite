c796a76173306336d4238617255b336071b9d57e
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-23 20:14:56 +0100

    Worked on the document component for the view applications page

---
+++ components/Document/index.tsx
@@ -0,0 +1,44 @@
+import styles from "./Document.module.css";
+
+interface DocumentItemProps {
+  status: string;
+  fileName: string;
+  url: string;
+}
+
+const DocumentItem: React.FC<DocumentItemProps> = ({
+  status,
+  fileName,
+  url,
+}) => {
+  const color =
+    status == "Being reviewed"
+      ? "#D9D9D9"
+      : status == "Accepted"
+      ? "#008000"
+      : "#FF0000";
+  return (
+    <div className={styles.container}>
+      <p>
+        <span className={styles.title}>Status:</span>
+        <span className={styles.value}>
+          <span
+            className={styles.dot}
+            style={{
+              backgroundColor: color,
+            }}
+          ></span>
+          <span style={{ color }}>{status}</span>
+        </span>
+      </p>
+      <p>
+        <span className={styles.title}>Document:</span>
+        <a className={styles.link} target="_blank" href={url}>
+          {fileName}
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default DocumentItem;
