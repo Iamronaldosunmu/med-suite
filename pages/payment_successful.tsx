aa31b9d21696b0ce2663321b0f8252a37ec6716b
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-23 20:40:51 +0100

    Finished working on the payment successful page

--- pages/payment_successful.tsx
+++ pages/payment_successful.tsx
@@ -1,7 +1,9 @@
+import { useRouter } from "next/router";
 import Logo from "../components/Logo";
 import styles from "../styles/PaymentSuccessful.module.css";
 
 const PaymentSuccessful = () => {
+  const router = useRouter();
   return (
     <main>
       <div className={styles.logoContainer}>
@@ -14,10 +16,13 @@ const PaymentSuccessful = () => {
         <p className={styles.bigText}>Payment Successful!</p>
         <p className={styles.smallText}>
           Your application has been successfully recieved and is being reviewed.
-          Please check in from time to time to see the progress of your application.
+          Please check in from time to time to see the progress of your
+          application.
         </p>
         <div className={styles.buttonGroup}>
-          <button>View Application</button>
+          <button onClick={() => router.push("/view_application")}>
+            View Application
+          </button>
           <button>Go Home</button>
         </div>
       </section>
