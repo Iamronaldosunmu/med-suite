60a15411185ef6b1cd45d71f5d84938d72aa607c
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2022-12-23 20:17:45 +0100

    Formatted navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -4,12 +4,12 @@ import MobileNav from "../MobileNav";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-    return (
-        <nav className={styles.navbarContainer}>
-            <Logo />
-            <MobileNav />
-            <DesktopNav />
-        </nav>
-    );
-}
+  return (
+    <nav className={styles.navbarContainer}>
+      <Logo />
+      <MobileNav />
+      <DesktopNav />
+    </nav>
+  );
+};
 export default Navbar;
