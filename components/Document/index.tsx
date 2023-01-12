6fae6cec6c8f8e660224cdd848258412fb11e9c3
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-01-12 23:54:07 +0100

    FInished working on the document component

--- components/Document/index.tsx
+++ components/Document/index.tsx
@@ -1,24 +1,140 @@
+import {
+  ChangeEvent,
+  Dispatch,
+  MouseEventHandler,
+  SetStateAction,
+  useRef,
+  useState,
+} from "react";
+import { useCookies } from "react-cookie";
+import client from "../../pages/api/Services/AxiosClient";
+import DocumentProps from "../../Types/Document";
+import LottieAnimation from "../Lottie";
+import Loader from "../Lottie/loading_6.json";
 import styles from "./Document.module.css";
 
 interface DocumentItemProps {
   status: string;
-  fileName: string;
+  fileName?: string;
   url: string;
+  fieldName:
+    | "nursingDegree"
+    | "practicingLicense"
+    | "photoIdentification"
+    | "evidenceOfRegistration"
+    | "resume"
+    | "referenceLetter"
+    | "birthCertificate"
+    | "proofOfWork"
+    | "profilePicture";
+  public_id: string;
+  documents: any[];
+  setDocuments: Dispatch<SetStateAction<any[]>>;
 }
+const defaultDocument = {
+  secure_url: "",
+  public_id: "",
+  fileName: "",
+};
 
 const DocumentItem: React.FC<DocumentItemProps> = ({
   status,
   fileName,
   url,
+  fieldName,
+  documents,
+  setDocuments,
+  public_id,
 }) => {
   const color =
-    status == "Being reviewed"
+    status == "Being Reviewed"
       ? "#D9D9D9"
       : status == "Accepted"
       ? "#008000"
       : "#FF0000";
+
+  const fieldNameToDocumentName = {
+    nursingDegree: "Nursing Degree",
+    practicingLicense: "Practicing License",
+    photoIdentification: "Photo Identification",
+    evidenceOfRegistration: "Evidence Of Registration",
+    resume: "Resume",
+    referenceLetter: "Reference Letter",
+    birthCertificate: "Birth Certificate",
+    profilePicture: "Profile Picture",
+    proofOfWork: "Proof Of Work",
+  };
+  const [loading, setLoading] = useState(false);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [cookies, setCookie, removeCookie] = useCookies(["user", "applicant"]);
+  const [file, setFile] = useState<DocumentProps>(defaultDocument);
+
+  console.log(cookies.applicant);
+  const onFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    setLoading(true);
+    try {
+      // Upload the file to cloudinary
+      const file = e.currentTarget.files![0];
+      let formData = new FormData();
+      formData.append("image", e.currentTarget.files![0]);
+      const { data } = await client.post("/image", formData);
+      const payload: any = {};
+      payload[fieldName] = {
+        ...data,
+        fileName: file.name,
+      };
+
+      // Delete the current Document
+      let response = await client.delete(`/image/${public_id}`);
+
+      // Handle the re-upload of the file on the backend
+      let response2 = await client.post(
+        `/review_documents/reupload/${cookies.user?.applicantId}`,
+        payload
+      );
+
+      const documentsCopy = documents;
+      let index = documentsCopy.findIndex(
+        (document) => document.fieldName == fieldName
+      );
+      console.log(index);
+      documentsCopy[index] = {
+        status: "Being Reviewed",
+        fieldName,
+        ...payload[fieldName],
+      };
+      console.log(documentsCopy[index]);
+      setDocuments(documentsCopy);
+      const cookiesCopy = cookies.applicant;
+      cookiesCopy.doumentReviewStatuses[fieldName] = "Being Reviewed";
+      cookiesCopy.documents[fieldName] = payload[fieldName];
+      setCookie("applicant", cookiesCopy);
+
+      console.log(response2);
+    } catch (error: any) {
+      console.log(error);
+      if (!error.response) alert("Please Check your Internet Connection🥲");
+      else {
+        alert(error.response?.data?.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const onUploadButtonClick = (e: any) => {
+    fileInputRef.current!.click();
+  };
+
+  // const;
   return (
     <div className={styles.container}>
+      <p>
+        <span className={styles.title}>Document Type:</span>
+        <a target="_blank" rel="noreferrer" href={url}>
+          {fieldNameToDocumentName[fieldName]}
+        </a>
+      </p>
       <p>
         <span className={styles.title}>Status:</span>
         <span className={styles.value}>
@@ -31,12 +147,34 @@ const DocumentItem: React.FC<DocumentItemProps> = ({
           <span style={{ color }}>{status}</span>
         </span>
       </p>
-      <p>
-        <span className={styles.title}>Document:</span>
-        <a className={styles.link} target="_blank" rel="noreferrer" href={url}>
-          {fileName}
-        </a>
-      </p>
+      {status !== "Rejected" && (
+        <p>
+          <span className={styles.title}>Document:</span>
+          <a
+            className={styles.link}
+            target="_blank"
+            rel="noreferrer"
+            href={url}
+          >
+            {fileName}
+          </a>
+        </p>
+      )}
+      {status == "Rejected" && (
+        <button onClick={onUploadButtonClick}>
+          {!loading && <span>Re-Upload Document</span>}
+          {loading && (
+            <LottieAnimation animationData={Loader} width={30} height={30} />
+          )}
+          <input
+            accept=".jpg,.jpeg,.png,.pdf"
+            onChange={onFileChange}
+            style={{ display: "none" }}
+            ref={fileInputRef}
+            type="file"
+          />
+        </button>
+      )}
     </div>
   );
 };
