4830c2ecabec5f8af0ab2109678d93d963dc0536
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-01-16 15:07:58 +0100

    Finished working on the chat button component

---
+++ components/ChatButton/index.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import navbarStyles from "../Navbar/Navbar.module.css"
+
+const ChatButton = () => {
+    const router = useRouter();
+    return (
+        <div onClick={() => router.push("/view_application/messages")} className={navbarStyles.chatButtonContainer}> 
+            <div className={navbarStyles.newMessagesDot}></div>
+            <img className={navbarStyles.chatImage} src="/icons/chat.svg" />
+        </div>
+    );
+}
+
+export default ChatButton;
