a3369b762406351c070e5ae153ffe7c553fb47de
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-01-16 15:12:13 +0100

    Changed the amound to be paid to med-suite

--- pages/application_form/application_fee.tsx
+++ pages/application_form/application_fee.tsx
@@ -19,7 +19,7 @@ const ApplicationFee = () => {
   });
   const config = {
     email: cookies.user?.email,
-    amount: 1500000,
+    amount: 3000000,
     publicKey: process.env.NEXT_PUBLIC_PS_PUBLIC_KEY!,
   };
   const handlePaystackCloseAction = () => {
@@ -43,7 +43,7 @@ const ApplicationFee = () => {
   };
   const componentProps = {
     ...config,
-    text: "Pay NGN 15,000",
+    text: "Pay NGN 30,000",
     onSuccess: handlePaystackSuccessAction,
     onClose: handlePaystackCloseAction,
   };
