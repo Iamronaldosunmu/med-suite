93ecf89e042dfd8b29e3cc4eb7ca81ba8aeda993
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-01-18 16:32:45 +0100

    Finished working on the MobileNav component

--- components/MobileNav/index.tsx
+++ components/MobileNav/index.tsx
@@ -1,12 +1,41 @@
 import styles from "./MobileNav.module.css";
+import { motion } from "framer-motion";
+import { Dispatch, SetStateAction } from "react";
 
-const MobileNav = () => {
-    return (
-        <div className={styles.mobilenavContainer}>
-            <div className={styles.topRectangle}></div>
-            <div className={styles.bottomRectangle}></div>
-        </div>
-    );
+interface MobileNavProps {
+  setMobileNavIsOpen: Dispatch<SetStateAction<boolean>>;
+  mobileNavIsOpen: boolean;
 }
+
+const MobileNav: React.FC<MobileNavProps> = ({
+  setMobileNavIsOpen,
+  mobileNavIsOpen,
+}) => {
+  return (
+    <>
+      <div
+        onClick={() => setMobileNavIsOpen(!mobileNavIsOpen)}
+        className={styles.mobilenavContainer}
+      >
+        <motion.div
+          animate={
+            !mobileNavIsOpen
+              ? { width: 38, height: 3 }
+              : { rotateZ: 45, x: -9.5, y: 5 }
+          }
+          className={styles.topRectangle}
+        ></motion.div>
+        <motion.div
+          animate={
+            !mobileNavIsOpen
+              ? { width: 26, height: 3 }
+              : { width: 38, rotateZ: -45, x: -10, y: -4 }
+          }
+          className={styles.bottomRectangle}
+        ></motion.div>
+      </div>
+    </>
+  );
+};
 
 export default MobileNav;
