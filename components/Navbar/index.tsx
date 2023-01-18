1134cdd13e6f36a99c714e1e06c2ec8d96a89c19
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-01-18 16:33:52 +0100

    Refactored the NavBar to have space for the message button

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,15 +1,63 @@
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import ChatButton from "../ChatButton";
+import ContactUsModal from "../ContactUsModal";
 import DesktopNav from "../DesktopNav";
 import Logo from "../Logo";
 import MobileNav from "../MobileNav";
 import styles from "./Navbar.module.css";
 
-const Navbar = () => {
+interface NavbarProps {
+  hasMessageButton?: boolean;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ hasMessageButton }) => {
+  const [mobileNavIsOpen, setMobileNavIsOpen] = useState(false);
+  const [contactModalShowing, setContactModalShowing] = useState(false);
+  const router = useRouter();
   return (
-    <nav className={styles.navbarContainer}>
-      <Logo />
-      <MobileNav />
-      <DesktopNav />
-    </nav>
+    <>
+      <nav className={styles.navbarContainer}>
+        <Logo />
+        <div className={styles.rightBoxContainer}>
+          {hasMessageButton && <ChatButton />}
+        <MobileNav
+          mobileNavIsOpen={mobileNavIsOpen}
+          setMobileNavIsOpen={setMobileNavIsOpen}
+        />
+
+        </div>
+        <DesktopNav hasMessageButton={hasMessageButton} setContactModalShowing={setContactModalShowing} />
+      </nav>
+      <div
+        style={{ maxHeight: mobileNavIsOpen ? 120 : 0 }}
+        className={styles.navItemsContainer}
+      >
+        <p
+          onClick={() => {
+            router.push("/");
+            setMobileNavIsOpen(false);
+          }}
+        >
+          Home
+        </p>
+        <p onClick={() => router.push("/login")}>Login</p>
+        <p
+          onClick={() => {
+            setMobileNavIsOpen(false);
+            setContactModalShowing(true);
+          }}
+        >
+          Contact Us
+        </p>
+      </div>
+      <AnimatePresence>
+        {contactModalShowing && (
+          <ContactUsModal setShowing={setContactModalShowing} />
+        )}
+      </AnimatePresence>
+    </>
   );
 };
 export default Navbar;
