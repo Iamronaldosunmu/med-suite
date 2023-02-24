1740c4915925884f31885dfe9b722e30b341567f
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-02-24 11:36:15 +0100

    Added global state for the chatboxes

--- components/Context/AdminContext.jsx
+++ components/Context/AdminContext.jsx
@@ -1,20 +1,31 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import client from "../../pages/api/Services/AxiosClient";
+import { toast } from "react-toastify";
+
 
 
 export const AdminContext = createContext();
 
 
 const AdminContextProvider = ({ children }) => {
+  // State for the applicant data
     const [applicantNumbers, setApplicantNumbers] = useState({
         total_applications: 0,
         being_reviewed: 0,
         approved_applications: 0,
     });
     const [applicantNumbersLoading, setApplicantNumbersLoading] = useState(true);
-  const [applicants, setApplicants] = useState([]);
+    const [applicants, setApplicants] = useState([]);
   const [applicantInReview, setApplicantInReview] = useState();
+  const [currentFieldName, setCurrentFieldName] = useState("")
+    
+  // State for the chatboxes
+    const [chatBoxes, setChatboxes] = useState([]);
+  const [loadingChatboxes, setLoadingChatboxes] = useState(false);
+  const [selectedChatbox, setSelectedChatbox] = useState("");
 
+  // State for the modals
+  const [rejectDocumentShowing, setRejectDocumentShowing] = useState(false);
 
     // Fetch Applicant Statistics from the backend
     const fetchStatistics = async () => {
@@ -24,21 +35,21 @@ const AdminContextProvider = ({ children }) => {
         setApplicantNumbers(data);
         setApplicantNumbersLoading(false);
       } catch (err) {
-        if (err.response.status == 500)
+        if (err.response?.status == 500)
           alert("Something went wrong on the server 🥲");
         else {
           alert("Could not Fetch data");
         }
       }
-    };
+  };
 
     // Fetch All the Applicants in the database
     const fetchAllApplicants = async () => {
         try {
             const { data } = await client.get("/applicant");
-            setApplicants(data.applicants);
+            setApplicants(data.applicants.filter(applicant => applicant.status !== "In Progress"));
         } catch (err) {
-          if (err.response && err.response.status == 500)
+          if (err.response && err.response?.status == 500)
             alert("Something went wrong on the server 🥲");
           else {
             alert("Could not Fetch data");
@@ -54,9 +65,58 @@ const AdminContextProvider = ({ children }) => {
     applicantCopy.doumentReviewStatuses[fieldName] = "Accepted";
     if (status == 200) setApplicantInReview(applicantCopy);
   }
+  
+  const rejectDocument = async (fieldName, message) => {
+    const payload = { fieldName, message };
+    try {
+      const { data, status } = await client.post(`/review_documents/reject/${applicantInReview._id}`, payload);
+      let applicantCopy = { ...applicantInReview }
+      applicantCopy.doumentReviewStatuses[fieldName] = "Rejected";
+      if (status == 200) setApplicantInReview(applicantCopy);
+      toast.success(`${fieldName} Document Rejected`);
+    } catch (err) {
+      console.log(err)
+      if (err.response && err.response.status == 500)
+      alert("Something went wrong on the server 🥲");
+    else {
+      alert("Could not Reject Document");
+    }
+    }
+    return true;
+
+
+  }
+
+  const fetchChatboxes = async () => {
+    try {
+      setLoadingChatboxes(true);
+      const { data : { chats } } = await client.get("/messages");
+              chats.sort(function(chatbox1 , chatbox2 ){return chatbox2.messages
+          .filter((message) => message.from == "applicant")
+          .filter(
+            (message) => message.status.applicant == "unread"
+          ).length - chatbox1.messages
+          .filter((message) => message.from == "applicant")
+          .filter(
+            (message) => message.status.applicant == "unread"
+          ).length});
+      setChatboxes(chats);
+      console.log(chats)
+    } catch (error) {
+      console.log(error)
+      alert("Could Not fetch the chatboxes!");
+    } finally {
+      setLoadingChatboxes(false);
+    }
+  };
+
+  useEffect(() => {
+    setInterval(() => fetchChatboxes(), 5000);
+  }, []);
+
 
     return (
-        <AdminContext.Provider value={{applicantNumbers, applicantNumbersLoading, fetchStatistics, fetchAllApplicants, applicants, applicantInReview, setApplicantInReview, approveDocument}}>
+        <AdminContext.Provider value={{applicantNumbers, applicantNumbersLoading, fetchStatistics, fetchAllApplicants, applicants, applicantInReview, setApplicantInReview, approveDocument, rejectDocument, fetchChatboxes, chatBoxes, selectedChatbox, setSelectedChatbox, rejectDocumentShowing, setRejectDocumentShowing, currentFieldName, setCurrentFieldName}}>
             {children}
         </AdminContext.Provider>
     )
